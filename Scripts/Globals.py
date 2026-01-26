6868d5bdaec93ebd5a4a4c8f6c411774106415e2
DICEALPHAHU <98395726+DICEALPHAHU@users.noreply.github.com>
2026-01-26 15:05:12 +0800

    更新 Globals.py ，适配全新的status.py中绘制统计图的逻辑

--- Scripts/Globals.py
+++ Scripts/Globals.py
@@ -3,6 +3,15 @@ from Scripts.Config import config
 uuid_caches: dict[str, str] = {}
 cpu_occupation: dict[str, list] = {}
 ram_occupation: dict[str, list] = {}
+tps_occupation: dict[str, list] = {} 
+mspt_occupation: dict[str, list] = {} 
+# 采集X轴真实时间
+cpu_time: dict[str, list] = {}  
+ram_time: dict[str, list] = {}  
+tps_time: dict[str, list] = {}  
+mspt_time: dict[str, list] = {} 
+MAX_HISTORY_LENGTH = 10  
+# ========================================================
 
 render_template = None
 
