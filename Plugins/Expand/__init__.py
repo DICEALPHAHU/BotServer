e061953f9286f1b120a39eec655543651f751894
DICEALPHAHU <98395726+DICEALPHAHU@users.noreply.github.com>
2026-02-06 18:13:10 +0800

    更新 __init__.py 加入全新功能 订阅B站up主的最新视频动态

--- Plugins/Expand/__init__.py
+++ Plugins/Expand/__init__.py
@@ -6,3 +6,5 @@ if config.ai_enabled:
     import_module('Plugins.Expand.Ai')
 if config.auto_reply_enabled:
     import_module('Plugins.Expand.Keywords')
+if config.bili_watcher_enabled:
+    import_module('Plugins.Expand.Biliwatcher')
